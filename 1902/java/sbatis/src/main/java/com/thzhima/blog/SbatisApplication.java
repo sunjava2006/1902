2656a120e062f8dca22660735a5e8a2b115d5ab2
sunjava2006 <jkwangrui@126.com>
2019-07-17 11:56:22 +0800

    7-17

--- 1902/java/sbatis/src/main/java/com/thzhima/blog/SbatisApplication.java
+++ 1902/java/sbatis/src/main/java/com/thzhima/blog/SbatisApplication.java
@@ -1,5 +1,7 @@
 package com.thzhima.blog;
 
+import java.util.List;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,8 +27,15 @@ public class SbatisApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		//int count = this.mapper.add(new RegUser(null, "谢大侠", "abc","女"));
 		//System.out.println("affect : "+count);
+		System.out.println(this.mapper.update("LaLa", "abc", 2));
+		
 		RegUser u = this.mapper.findByID(2);
 		System.out.println(u);
+//		
+//		List<RegUser> list = this.mapper.listAll();
+//		System.out.println(list);
+//		System.out.println("===============");
+//		System.out.println(this.mapper.list(1));
 		
 	}
 
--- 1902/java/sbatis/src/main/java/com/thzhima/blog/mapper/RegUserMapper.java
+++ 1902/java/sbatis/src/main/java/com/thzhima/blog/mapper/RegUserMapper.java
@@ -1,23 +1,40 @@
 package com.thzhima.blog.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.thzhima.blog.bean.RegUser;
 
 @Mapper
 public interface RegUserMapper {
-   
+	@Results(
+		  id = "RegUserMap",
+		  value={@Result(id=true, property="userID", column="user_id"),
+		  @Result(property = "regName", column = "reg_name")
+    })
 	@Select("select * from reg_user where user_id=#{id}")
-	@Results({@Result(id=true, property="userID", column="user_id"),
-			  @Result(property = "regName", column = "reg_name")
-	})
 	public RegUser findByID(int id);
+	
+	@ResultMap(value = "RegUserMap")
+	@Select("select * from reg_user")
+	public List<RegUser> listAll();
+	
+	@Select("select * from reg_user where user_id=#{id}")
+	public Map list(int id);
+	
 	
 	@Insert("insert into reg_user(reg_name, pwd, gender) values (#{regName}, #{pwd}, #{gender})")
 	public int add(RegUser user);
+	
+	@Update("update reg_user set reg_name=#{regName}, pwd=#{pwd} where user_id=#{userID}")
+	public int update(String regName, String pwd, int userID);
 	
 }
