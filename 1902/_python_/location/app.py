94eb06e303d57a238b9da157a98f5c7698782aad
sunjava2006 <jkwangrui@126.com>
2019-08-16 16:07:05 +0800

    8-16

---
+++ 1902/_python_/location/app.py
@@ -0,0 +1,22 @@
+from flask import Flask ,jsonify
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "hello"
+
+@app.route('/hi/<name>')
+def hi(name):
+    return 'hi:'+name.upper()
+
+@app.route('/location/<latitude>/<longitude>/<key>')
+def location(latitude, longitude, key):
+    return jsonify({'latitude':latitude, 'longitude':longitude, 'key':key, 'name':'便利蜂'}),\
+    {"responseContent":"application/json;charset=utf-8"}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
