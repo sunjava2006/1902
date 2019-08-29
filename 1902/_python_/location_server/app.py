5217b77b9c9ae43634baaae13c3377672449b095
sunjava2006 <jkwangrui@126.com>
2019-08-29 14:09:54 +0800

    8-29

--- 1902/_python_/location_server/app.py
+++ 1902/_python_/location_server/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, Response, redirect
+import db 
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def login():
+def index():
     return render_template('/login.html')
+
+
+@app.route('/main')
+def main():
+    return render_template('/main.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    print('login-----------')
+    userName = request.values.get('userName')
+    pwd = request.values.get('pwd')
+    data = db.login(userName, pwd)
+    if data:
+        return redirect('/main')
+    else:
+        return render_template('/login.html', msg='登录失败，重试。')
+
 
 
 if __name__ == "__main__":
---
+++ 1902/_python_/location_server/db.py
@@ -0,0 +1,49 @@
+import cx_Oracle as orl
+import os
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'  
+
+def getconn():
+    return orl.connect('location/location@127.0.0.1/xe')
+
+
+def login(user_name, pwd):
+    sql = 'select * from users where user_name=:user_name and pwd=:pwd'
+    return select(sql, one=False, user_name=user_name, pwd=pwd)
+
+
+def select(sql, one=True, **arg):
+    data = None
+    try:
+        conn = getconn()
+        cur = conn.cursor()
+        cur.execute(sql, arg)
+        if one:
+            data = cur.fetchone()
+        else:
+            data = cur.fetchall()
+        conn.commit()
+    except Exception as identifier:
+        if conn:
+            conn.rollback()
+        print(identifier)
+    finally:
+        if cur:
+            cur.close()
+        if conn:
+            conn.close()
+    return data
+
+
+
+def sum(a, b, *c, **d):
+    print(d)
+    result = 0
+    result += (a+b)
+    for i in c:
+        result += i
+    for i in d.keys():
+        result += d.get(i)
+    return result
+
+
+print(login('wang', '123456'))
