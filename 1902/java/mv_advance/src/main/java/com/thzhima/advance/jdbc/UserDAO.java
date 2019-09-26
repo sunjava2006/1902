2356260bfbc75959bc742fdf13fff098810ab900
sunjava2006 <jkwangrui@126.com>
2019-09-26 15:45:30 +0800

    9-26

--- 1902/java/mv_advance/src/main/java/com/thzhima/advance/jdbc/UserDAO.java
+++ 1902/java/mv_advance/src/main/java/com/thzhima/advance/jdbc/UserDAO.java
@@ -7,8 +7,19 @@ import java.sql.SQLException;
 
 import com.thzhima.advance.entity.User;
 
+/**
+ * @author wangrui
+ *
+ */
 public class UserDAO {
 
+	/**
+	 * 根据用户名和密码，查询用户。
+	 * @param userName 用户名
+	 * @param pwd   密码
+	 * @return 当能够查询到用户时，返回User对象。否则返回null。
+	 * @throws SQLException
+	 */
 	public User find(String userName, String pwd) throws SQLException {
 		User u = null;
 		String sql = "select * from t_users "
@@ -47,7 +58,81 @@ public class UserDAO {
 
 		return u;
 	}
+
+
+	public boolean add(User u) throws SQLException {
+		boolean ok = false;
+		String sql = "insert into t_users(id,user_name,pwd)values(seq_users.nextval,?,?)";
+		Connection conn = null;
+		PreparedStatement stm = null;
+		try {
+			conn = ConnectionUtil.getConnection();
+			conn.setAutoCommit(false);
+			stm = conn.prepareStatement(sql);
+			stm.setString(1, u.getUserName()); // 设置参数，索引从1开始。
+			stm.setObject(2, u.getPwd());
+			
+			int count = stm.executeUpdate();// 所有DDL操作，都调用executeUpdate()方法。
+			if(1 == count) {
+				ok = true;
+			}
+			conn.commit();
+		}catch(SQLException ex) {
+			conn.rollback();
+			ex.printStackTrace();
+		}finally {
+			if(null == stm) {
+				stm.close();
+			}
+			if(null != conn) {
+				conn.setAutoCommit(true);
+				conn.close();
+			}
+		}
+		return ok;
+	}
 	
+	public boolean deleteByID(Integer ... ids) throws SQLException {
+		boolean ok = true;
+		String sql = "delete from t_users where id=?";
+		Connection conn = null;
+		PreparedStatement stm = null;
+		try {
+			conn = ConnectionUtil.getConnection();
+			conn.setAutoCommit(false);
+			stm = conn.prepareStatement(sql);
+			if(null != ids) {
+				for(int i=0;i<ids.length;i++) {
+					int id = ids[i];
+					stm.setInt(1, id);
+					int count = stm.executeUpdate();
+					if(count<0) {
+						ok = false;
+						conn.rollback();
+						break;
+					}
+				}
+			}
+			if(ok) {
+				conn.commit();
+			}
+			
+		} catch (SQLException e) {
+			conn.rollback();
+			ok = false;
+			e.printStackTrace();
+		} finally {
+			if(null == stm) {
+				stm.close();
+			}
+			if(null != conn) {
+				conn.setAutoCommit(true);
+				conn.close();
+			}
+		}
+		
+		return ok;
+	}
 	
 	
 }
