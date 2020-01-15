61ef149ff9c0f5a745d0ec693b574c4362f626c4
sunjava2006 <jkwangrui@126.com>
2020-01-15 10:36:39 +0800

    not new

--- 1904/java/java_base/src/com/thzhima/base/ch09/donotnew/Test.java
+++ 1904/java/java_base/src/com/thzhima/base/ch09/donotnew/Test.java
@@ -10,14 +10,24 @@ public class Test {
 	public static void main(String[] args) 
 			throws IOException, InstantiationException,
 			IllegalAccessException, ClassNotFoundException, NoSuchMethodException, SecurityException, IllegalArgumentException, InvocationTargetException {
-		
+		// 我们要的对象。
 		Car car = null;
 		
-		Properties p = new Properties();
+		Properties p = new Properties();// 创建配置文件对象。
+		
+		// 配置文件对象，读取指定的配置文件 
 		p.load(Test.class.getResourceAsStream("/cnf.properties"));
+		
+		// 从配置文件对象中，取参数的值。这个值就是我们想创建的类名。
 		String className = p.getProperty("car");
 		
-		Method method = Class.forName(className).getMethod("createNew", null);
+		// 加载类，获取类对象。为反射做准备
+		Class clazz = Class.forName(className);
+		
+		// （反射方法）获取类中定义的方法对象。
+		Method method = clazz.getMethod("createNew", null);
+		
+		// （反射方法）调用方法。
 		car  = (Car) method.invoke(null, null);
 		
 		car.run();
