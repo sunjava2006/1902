5da5a8de523f89cdcadc8bcc39e076d7a188ed28
sunjava2006 <jkwangrui@126.com>
2020-02-04 17:25:24 +0800

    oracle

---
+++ 1904/oracle/student.sql
@@ -0,0 +1,42 @@
+select * from students;
+
+update students SET student_id = 3 where student_name='李小红';
+
+commit;
+
+alter table students add constraint pk_students_id primary key(student_id);
+
+create table subjects
+(
+subject_id number(3),
+subject_name varchar2(20 char) constraint uni_subj_name unique,
+constraint pk_subjects_id primary key(subject_id)
+);
+
+create table scores
+(
+   student_id number constraint fk_scores_stuid references students(student_id),
+   subject_id number(3),
+   score number(3) not null
+);
+alter table scores add constraint fk_scores_subjid foreign key(subject_id) REFERENCES subjects(subject_id);
+alter table scores modify score not null ;
+DELETE from scores where subject_id=4;
+
+select * from user_constraints;
+
+insert into subjects values (3,'三年级数学');
+
+--============================================================
+select * from students;
+select * from subjects;
+select * from scores;
+insert into scores values (1,3,null);
+
+alter table scores add constraint ck_score check (score between 0 and 100);
+
+commit;
+
+insert into students(student_id, student_name)values(4,'李亮');
+
+
