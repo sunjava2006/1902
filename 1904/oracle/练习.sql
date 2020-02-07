b5f86a2193629f0202257c09f423c5d0d300490d
sunjava2006 <jkwangrui@126.com>
2020-02-07 15:37:26 +0800

    DML_Transaction

--- 1904/oracle/练习.sql
+++ 1904/oracle/练习.sql
@@ -78,17 +78,74 @@ where c.country_id='UK';
 
 
 -- 12、列出曾经在"Programmer"岗位工作过的员工，列出名字、当前岗位。
+select first_name,job_title from(
+select first_name, job_id from employees where employee_id in
+(select employee_id from job_history where job_id=(
+SELECT job_id from jobs WHERE job_title='Programmer')))a
+join jobs j on a.job_id=j.job_id;
+
 -- 13、列出各部门经理和他们管理的员工的名字。按经理名字排序。
+select b.manager_id,b.first_name, a.first_name from employees a join 
+(select employee_id manager_id, first_name from employees where employee_id in(
+select manager_id from departments)) b
+on a.manager_id=b.manager_id
+order by b.first_name;
+
 -- 14、分页查询，按员工的工作日期排序。查询出员工的名字、部门名、岗位名、工资。每页5条记录。查询第1页、第2页。
+select * from(
+select a.hire_date, a.first_name ,a.job_title,a.department_name,a.salary, rownum ro from
+(select e.*, d.*,j.* from employees e
+left join departments d on e.department_id=d.department_id
+left join jobs j on j.job_id=e.job_id
+order by e.hire_date desc)a where rownum<=10) where ro>5;
+
 -- 15、查询出各部门最高工资的人。
+select * from(
+select first_name, department_id, salary , rank() over(partition by department_id order by salary desc) NO
+from employees )where NO=1;
+
 -- 16、查询出各岗位最高工资的人。
+select * from(
+select first_name, job_id, salary , rank() over(partition by job_id order by salary desc) NO
+from employees )where NO=1 order by salary desc;
+
 -- 17、查询出各岗位规定的最高（最上限）工资的人。
+select first_name,job_title, salary from employees e
+join jobs j on e.job_id=j.job_id and e.salary=j.max_salary; 
+
 -- 18、查询出各部门中，员工工资，正好为本部门平均工资的人。
+select e.first_name,e.department_id,e.salary from employees e join
+(select department_id, round(avg(nvl(salary,0))) ag from employees group by department_id) b
+on e.department_id=b.department_id and e.salary=b.ag;
+
 -- 19、查询出员工中最高工资的员工所在的部门。
+select department_name from departments where department_id in
+(select department_id from employees 
+where salary=(
+select max(salary)from employees));
+
 -- 20、查询出员工中最高工资的员工所在的岗位、工资、该员工工资与该岗位最高工资之间的差额。
+select first_name, job_title, j.max_salary-salary from 
+(SELECT * fROM employees where salary = (select max(salary) from employees))a
+join jobs j on a.job_id=j.job_id;
+
 -- 21、查询出各岗位的员工，按岗位、员工工作日期倒序排列。
+select job_id,hire_date,first_name from employees order by job_id desc , hire_date desc;
+
 -- 22、查询出在London上班的员工.列出名字。
+select first_name,city from employees e
+ join departments d on e.department_id=d.department_id
+ join locations l on d.location_id= l.location_id
+where upper(city)='LONDON';
+
+
 -- 23、是否只有销售岗位的员工才有提成。查询返回是或没有结果。
+select 'have'from employees 
+where exists 
+(select * from employees where job_id not like'SA_%' and commission_pct is not null);
+
+--SELECT * FROM EMPLOYEES;
+
 -- 24、公司要为员工涨薪水50%。但不能超过各岗位的最高工资，如涨50%后超过限额的不涨。列出可以涨薪的员工、当前工资、加薪之后的工资、岗位工资上限。
 -- 25、查询出在两个以上岗位工作过的员工。列出曾工作过的岗位。
 -- 26、查询出各部门工资最高的2个人。
